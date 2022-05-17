7403a78abb35982d95ed786d3f173741dda062e4
mkumar0802 <mkumar0802@gmail.com>
2022-05-17 11:23:11 +0530

    login fix

--- src/components/admin/RegistrationPage.js
+++ src/components/admin/RegistrationPage.js
@@ -82,18 +82,18 @@ function Registerpage() {
                         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="Enter email" required />
                     </div>
                     <div class="mb-6">
-                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Your password</label>
                         <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="password" required />
                     </div>
                     <div class="mb-6">
-                        <label for="confirmPassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Confirm password</label>
+                        <label for="confirmPassword" class="block mb-2 text-sm font-medium text-gray-900 ">Confirm password</label>
                         <input value={confirmpassword} onChange={(e) => setConfirmPassword(e.target.value)} type="password" id="confirmPassword" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="confirm password" required />
                     </div>
                     <div class="flex items-start mb-6">
                         <div class="flex items-center h-5">
                             <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 " required />
                         </div>
-                        <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                        <label for="remember" class="ml-2 text-sm font-medium text-gray-900 ">Remember me</label>
                     </div>
 
                     <div className="flex  gap-5">
